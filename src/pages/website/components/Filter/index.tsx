fea8de34126b2aef34d612e4d113c3b6a7175f0d
dezhizhang <1541609448@qq.com>
2023-03-25 03:50:51 +0800

    feat:修改状态值

--- src/pages/website/components/Filter/index.tsx
+++ src/pages/website/components/Filter/index.tsx
@@ -10,7 +10,7 @@
 import React, { ChangeEvent } from 'react';
 import { Input, Select } from 'antd';
 import fcStyles from '@/styles/filter.less';
-import { WEBSITE_TYPE } from '../../constants';
+import { WEBSITE_TYPE, STATUS_TYPE } from '../../constants';
 import styles from './index.less';
 
 export interface FilterTableProps {
@@ -59,7 +59,7 @@ const FilterTable: React.FC<FilterTableProps> = (props) => {
             <th className={styles.labelArea}>状态：</th>
             <td>
               <Select style={{ width: '100%' }} placeholder="请选择网站状态" value={filter.status}>
-                {WEBSITE_TYPE.map((item) => (
+                {STATUS_TYPE.map((item) => (
                   <Select.Option value={item.value} key={item.value}>
                     {item.label}
                   </Select.Option>
