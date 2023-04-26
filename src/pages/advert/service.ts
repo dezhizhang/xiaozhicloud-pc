691ed166fbd069cbd3b968689c1c1c40a9e20b33
dezhizhang <1541609448@qq.com>
2023-04-26 12:34:07 +0800

    feat:更新广告接口

--- src/pages/advert/service.ts
+++ src/pages/advert/service.ts
@@ -1,9 +1,18 @@
+/*
+ * :file description:
+ * :name: /xiaozhicloud-pc/src/pages/advert/service.ts
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-04-26 01:37:22
+ * :last editor: 张德志
+ * :date last edited: 2023-04-26 12:29:46
+ */
 import request from '@/utils/request';
 import SERVICE_MAP from '@/utils/service';
 
 // 获取网站列表
 export async function getWebsiteList(params: any): Promise<Website.ResponseInfo> {
-  return request(SERVICE_MAP.WEBSITE_LIST, {
+  return request(SERVICE_MAP.ADVERT_LIST, {
     method: 'POST',
     data: params,
   });
@@ -11,7 +20,7 @@ export async function getWebsiteList(params: any): Promise<Website.ResponseInfo>
 
 // 新增网站
 export async function getWebsiteAdd(params: Website.RequestType): Promise<any> {
-  return request(SERVICE_MAP.WEBSITE_ADD, {
+  return request(SERVICE_MAP.ADVERT_ADD, {
     method: 'POST',
     data: params,
   });
@@ -19,7 +28,7 @@ export async function getWebsiteAdd(params: Website.RequestType): Promise<any> {
 
 //删除网站
 export async function getWebsiteDelete(params: { _id: string }): Promise<any> {
-  return request(SERVICE_MAP.WEBSITE_DELETE, {
+  return request(SERVICE_MAP.ADVERT_DELETE, {
     method: 'DELETE',
     data: params,
   });
@@ -27,7 +36,7 @@ export async function getWebsiteDelete(params: { _id: string }): Promise<any> {
 
 // 编辑网站
 export async function getWebsiteUpdate(params: any): Promise<any> {
-  return request(SERVICE_MAP.WEBSITE_UPDATE, {
+  return request(SERVICE_MAP.ADVERT_UPDATE, {
     method: 'PUT',
     data: params,
   });
--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,3 +1,12 @@
+/*
+ * :file description:
+ * :name: /xiaozhicloud-pc/src/utils/service.ts
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-04-26 01:37:22
+ * :last editor: 张德志
+ * :date last edited: 2023-04-26 12:29:17
+ */
 export const BASE_URL = '/api/v1';
 
 const SERVICE_MAP = {
@@ -6,12 +15,17 @@ const SERVICE_MAP = {
   MANAGER_ADD: `/manager/add`,
   MANAGER_UPDATE: `/manager/edit`,
   MANAGER_DELETE: `/manager/delete`,
-
   // 获取网站列表
   WEBSITE_LIST: `/website/list`,
   WEBSITE_ADD: `/website/add`,
   WEBSITE_DELETE: `/website/delete`,
   WEBSITE_UPDATE: `/website/edit`,
+
+  // 获取网站列表
+  ADVERT_LIST: `/advert/list`,
+  ADVERT_ADD: `/advert/add`,
+  ADVERT_DELETE: `/advert/delete`,
+  ADVERT_UPDATE: `/advert/edit`,
 
   // 办公文档
   OFFICE_LIST: `/office/list`,
