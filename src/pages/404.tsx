865509c2e17857f3464f1168ec5e1e65cca49f2a
dezhizhang <1541609448@qq.com>
2023-04-28 13:36:57 +0800

    feat:更新

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,12 @@
+/*
+ * :file description:
+ * :name: /xiaozhicloud-pc/src/pages/404.tsx
+ * :author: 张德志
+ * :copyright: (c) 2023, xiaozhi
+ * :date created: 2023-04-26 01:37:22
+ * :last editor: 张德志
+ * :date last edited: 2023-04-28 13:34:57
+ */
 import { Button, Result } from 'antd';
 import React from 'react';
 import { history } from 'umi';
@@ -6,10 +15,10 @@ const NoFoundPage: React.FC = () => (
   <Result
     status="404"
     title="404"
-    subTitle="Sorry, the page you visited does not exist."
+    subTitle="对不起你访问的页面没有找到"
     extra={
       <Button type="primary" onClick={() => history.push('/')}>
-        Back Home
+        返回首页
       </Button>
     }
   />
