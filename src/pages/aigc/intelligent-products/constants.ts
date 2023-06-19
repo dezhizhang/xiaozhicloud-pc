7346aca557224639a2c19cef788e0b81a6ebd6ce
dezhizhang <1541609448@qq.com>
2023-06-19 22:56:40 +0800

    feat:aigc添加菜单

--- src/pages/aigc/intelligent-products/constants.ts
+++ src/pages/aigc/intelligent-products/constants.ts
@@ -1,17 +1,22 @@
 /*
  * :file description:
- * :name: /xiaozhicloud-pc/src/pages/aigc/constants.ts
+ * :name: /xiaozhicloud-pc/src/pages/aigc/intelligent-products/constants.ts
  * :author: 张德志
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-25 12:44:12
  * :last editor: 张德志
- * :date last edited: 2023-05-26 17:37:34
+ * :date last edited: 2023-06-19 22:54:40
  */
 
 export const baseUrl = '/aigc/intelligent-products';
 
 export enum TOP_CLASSIFY_TYPE {
   WRITING_PLATFORM = 'writing-platform',
+  INTERACTIVE_PLATFORM = 'interactive-platform',
+  PAINTING_PLATFORM = 'painting-platform',
+  VISUAL_PLATFORM = 'visual-platform',
+  TELEVISION_PLATFORMS = 'television-platforms',
+  VOICE_PLATFORM = 'voice-platform',
 }
 
 export const TOP_CLASSIFY = [
@@ -21,23 +26,23 @@ export const TOP_CLASSIFY = [
   },
   {
     label: '互动平台',
-    value: 'interactive-platform',
+    value: TOP_CLASSIFY_TYPE.INTERACTIVE_PLATFORM,
   },
   {
     label: '绘画平台',
-    value: 'painting-platform',
+    value: TOP_CLASSIFY_TYPE.PAINTING_PLATFORM,
   },
   {
     label: '视觉平台',
-    value: 'visual-platform',
+    value: TOP_CLASSIFY_TYPE.VISUAL_PLATFORM,
   },
   {
     label: '影视平台',
-    value: 'television-platforms',
+    value: TOP_CLASSIFY_TYPE.TELEVISION_PLATFORMS,
   },
   {
     label: '语音平台',
-    value: 'voice-platform',
+    value: TOP_CLASSIFY_TYPE.VOICE_PLATFORM,
   },
   {
     label: '企业平台',
@@ -79,6 +84,7 @@ export const TOP_CLASSIFY = [
 
 // 二级分类
 export const SECONDARY_CLASSIFY = {
+  //写作平台
   [TOP_CLASSIFY_TYPE.WRITING_PLATFORM]: [
     {
       label: '写作工具',
@@ -95,6 +101,42 @@ export const SECONDARY_CLASSIFY = {
     {
       label: '内容检测',
       value: 'content-detection',
+    },
+  ],
+  //互动平台
+  [TOP_CLASSIFY_TYPE.INTERACTIVE_PLATFORM]: [
+    {
+      label: '聊天机器人',
+      value: 'chatbot',
+    },
+  ],
+  //绘画平台
+  [TOP_CLASSIFY_TYPE.PAINTING_PLATFORM]: [
+    {
+      label: '聊天机器人',
+      value: 'chatbot',
+    },
+  ],
+  //视觉平台
+  [TOP_CLASSIFY_TYPE.VISUAL_PLATFORM]: [
+    {
+      label: '视觉设计',
+      value: 'visual-design',
+    },
+    {
+      label: '头像化身',
+      value: 'avatar-design',
+    },
+  ],
+  //影视平台
+  [TOP_CLASSIFY_TYPE.TELEVISION_PLATFORMS]: [
+    {
+      label: '视频生成',
+      value: 'video-generation',
+    },
+    {
+      label: '数字虚拟人',
+      value: 'digital-virtual-humans',
     },
   ],
 };
