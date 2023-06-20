63ecdaf9b715ab4b1bb628d3d55f8cd1c5214c09
dezhizhang <1541609448@qq.com>
2023-06-20 22:32:58 +0800

    feat:添加网站详情

--- src/pages/website/[type]/components/Header/index.tsx
+++ src/pages/website/[type]/components/Header/index.tsx
@@ -1,20 +1,33 @@
 /*
  * :file description:
- * :name: /xiaozhicloud-pc/src/pages/aigc/[type]/components/Header/index.tsx
+ * :name: /xiaozhicloud-pc/src/pages/website/[type]/components/Header/index.tsx
  * :author: 张德志
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-26 18:31:49
  * :last editor: 张德志
- * :date last edited: 2023-05-26 18:54:05
+ * :date last edited: 2023-06-20 22:31:52
  */
 import React from 'react';
 import styles from './index.less';
-import { Button } from 'antd';
+import { Descriptions } from 'antd';
 
 const Header: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}></div>
+      <div className={styles.wrapper}>
+        <Descriptions
+          title="网站详情"
+          bordered
+          column={{ xxl: 3, xl: 3, lg: 3, md: 3, sm: 3, xs: 3 }}
+        >
+          <Descriptions.Item label="标题">Cloud Database</Descriptions.Item>
+          <Descriptions.Item label="类型">Prepaid</Descriptions.Item>
+          <Descriptions.Item label="封面">18:00:00</Descriptions.Item>
+          <Descriptions.Item label="创建时间">$80.00</Descriptions.Item>
+          <Descriptions.Item label="状态">$20.00</Descriptions.Item>
+          <Descriptions.Item label="描述">$60.00</Descriptions.Item>
+        </Descriptions>
+      </div>
     </div>
   );
 };
