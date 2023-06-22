b8616faf94496ef14addab4eacce3418eaa2ff99
dezhizhang <1541609448@qq.com>
2023-06-22 09:23:55 +0800

    feat:人工智能产品

--- src/pages/aigc/news-information/[type]/index.tsx
+++ src/pages/aigc/news-information/[type]/index.tsx
@@ -6,14 +6,14 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-26 17:36:27
  * :last editor: 张德志
- * :date last edited: 2023-06-19 09:21:08
+ * :date last edited: 2023-06-22 09:21:49
  */
 import React, { useRef, useEffect, useState } from 'react';
 import styles from './index.less';
 import { useParams } from 'umi';
 import { Button } from 'antd';
 import { getDetailInfo } from '../service';
-import Header from './components/Header';
+import Header from '@/components/Header';
 import DetailDrawer from './components/DetailDrawer';
 
 const AigcDetail: React.FC = () => {
